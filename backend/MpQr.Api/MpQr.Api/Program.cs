d787b070f074becc1f5affcd1379bc5e58649836
LAPTOP-JI5MPNI3\flora <floraavalos469@gmail.com.ar>
2026-03-20 15:40:28 -0300

    se habilita swagger en produccion

--- backend/MpQr.Api/MpQr.Api/Program.cs
+++ backend/MpQr.Api/MpQr.Api/Program.cs
@@ -34,11 +34,14 @@ builder.Services.AddScoped<StorePaymentRepository>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseRouting();
